5534b0775b34140d5e12138d9479f0080b65bd2b
Ondrej Maxian <ondrejmaxian@pop-os.localdomain>
2023-09-18 17:38:43 -0500

    Update on PAR23

--- MyosinModels/PAR2PAR3Myosin.m
+++ MyosinModels/PAR2PAR3Myosin.m
@@ -11,23 +11,20 @@ ATot = 50;
 kpA = 2/ATot*koffA; % First number is Kp_hat
 kAplus = 60/500; % very unknown
 %PAR-2
-DP = 0.1;
+DP = 0.15;
 konP = 0.13;
 koffP = 7.3e-3;
-PTot = ATot;
-kdpP = kdpA;
-kpP = kpA;
-rAP = 0.01; % A inhibiting P
+PTot = 50;
+rAP = 1e-2; % A inhibiting P
 rPA = rAP; % P inhibiting A
 % Myosin
 DM = 0.05;
-konM = 4; % high so we recruit myosin
+konM = 2; % high so we recruit myosin
 koffM = 0.12;
 eta = 0.1;
 gamma = 1e-3;
-Mtot = 50;
 Sigma0 = 1.1e-3;
-rPM = 0.01;
+rPM = 1e-2;
 % Dimensionless
 DA_Hat = DA/(L^2*koffA);
 KonA_Hat = konA/(koffA*h);
@@ -36,8 +33,6 @@ KpA_Hat = kpA*ATot/koffA;
 Kf_Hat = kAplus*ATot/konA;
 DP_Hat = DP/(L^2*koffA);
 KonP_Hat = konP/(koffA*h);
-KdpP_Hat = kdpP/koffA;
-KpP_Hat = kpP*PTot/koffA;
 KoffP_hat = koffP/koffA;
 SigmaHat =Sigma0/sqrt(eta*gamma)/(L*koffA);
 DM_Hat = DM/(koffA*L^2);
@@ -46,7 +41,7 @@ koffM_Hat = koffM/koffA;
 LRatio = sqrt(eta/gamma)/L;
 
 % Initialization
-dt=1e-2;
+dt=1e-1;
 N=100;
 dx = 1/N;
 DSq = SecDerivMat(N,dx);
@@ -54,21 +49,21 @@ DOneCenter = FirstDerivMatCenter(N,dx);
 x = (0:N-1)'*dx;
 advorder = 1;
 % Start with small zone of PAR-2 on posterior cap
-cap = (x > 0.65 & x < 0.85);
+cap = (x > 0.8 & x < 0.85);
 A1 = 0.5*ones(N,1).*(~cap);
 A2 = 0.25*ones(N,1).*(~cap);
-P1 = 0.5*ones(N,1).*cap;
-P2 = 0.25*ones(N,1).*cap;
-M = 0.5*ones(N,1);
-plot(x,A1+2*A2,x,P1+2*P2,x,M)
+P = ones(N,1).*cap;
+M = 0.5*ones(N,1);%+0.4*(rand(N,1)-0.5);
+%plot(x,Ass,':',x,Pss,':',x,Mss,':')
+%hold on
 
 er = 1;
 nIts = 0;
 while (er > 1e-10)
     t = nIts*dt;
-    A1prev = A1; P1prev = P1; P2prev = P2; A2prev = A2; Mprev=M;
+    A1prev = A1; A2prev = A2; Pprev = P; Mprev=M;
     Ac = 1 - sum(A1+2*A2)*dx;
-    Pc = 1 - sum(P1+2*P2)*dx;
+    Pc = 1 - sum(P)*dx;
     Mc = 1 - sum(M)*dx;
 
     % Flows
@@ -78,32 +73,30 @@ while (er > 1e-10)
     % Advection (explicit)
     MinusdxMv = AdvectionRHS(t,M,dx,vHalf,advorder);
     MinusdxA1v = AdvectionRHS(t,A1,dx,vHalf,advorder);
-    MinusdxP1v = AdvectionRHS(t,P1,dx,vHalf,advorder);
     MinusdxA2v = AdvectionRHS(t,A2,dx,vHalf,advorder);
-    MinusdxP2v = AdvectionRHS(t,P2,dx,vHalf,advorder);
+    MinusdxPv = AdvectionRHS(t,P,dx,vHalf,advorder);
 
     % Reactions
     Feedback = PAR3FeedbackFcn(A1+2*A2);
-    RHS_M = SigmaHat*MinusdxMv + konM_Hat*Mc - (koffM_Hat+rPM.*PTot*(P1+2*P2)/koffA).*M;
+    RHS_M = SigmaHat*MinusdxMv + konM_Hat*Mc - (koffM_Hat+rPM.*PTot*P/koffA).*M;
     RHS_A1 = SigmaHat*MinusdxA1v + KonA_Hat*(1+Kf_Hat*Feedback)*Ac - A1 ...
         + 2*KdpA_Hat*A2 - 2*KpA_Hat*A1.^2;
-    RHS_A2 = SigmaHat*MinusdxA2v - KdpA_Hat*A2 + KpA_Hat*A1.^2 - rPA*PTot/koffA*(P1+2*P2).*A2;
-    RHS_P1 = SigmaHat*MinusdxP1v + KonP_Hat*Pc - KoffP_hat*P1 + 2*KdpP_Hat*P2 ...
-        - 2*KpP_Hat*P1.^2 - rAP*ATot/koffA*(A1+2*A2).*P1;
-    RHS_P2 = SigmaHat*MinusdxP2v - KdpP_Hat*P2 + KpP_Hat*P1.^2 - rAP*ATot/koffA*(A1+2*A2).*P2;
-    P1 = (speye(N)/dt-DP_Hat*DSq) \ (P1/dt+RHS_P1);
-    P2 = P2 + dt*RHS_P2;
+    RHS_A2 = SigmaHat*MinusdxA2v - KdpA_Hat*A2 + KpA_Hat*A1.^2 - rPA*PTot/koffA*P.*A2;
+    RHS_P = SigmaHat*MinusdxPv + KonP_Hat*Pc - KoffP_hat*P - rAP*ATot/koffA*(A1+2*A2).*P;
+    P = (speye(N)/dt-DP_Hat*DSq) \ (P/dt+RHS_P);
     A1 = (speye(N)/dt-DA_Hat*DSq) \ (A1/dt+RHS_A1);
     A2 = A2 + dt*RHS_A2;
     M  = (speye(N)/dt-DM_Hat*DSq) \ (M/dt+RHS_M);
     chk = (M-Mprev)/dt- (DM_Hat*DSq*M + RHS_M);
-    mv = [P1-P1prev; A1-A1prev; P2-P2prev; A2-A2prev]/dt;
+    mv = [P-Pprev; A1-A1prev;A2-A2prev]/dt;
     er = max(abs(mv));
     if (mod(nIts,100)==0)
-        plot(x,A1,x,2*A2,x,P1,x,2*P2,x,M)
+        hold off
+        plot(x,A1+2*A2,x,P,x,M)
         drawnow
     end
     nIts=nIts+1;
 end
 %figure
-%plot(x,A1+2*A2,x,P1+2*P2,x,M)
+set(gca,'ColorOrderIndex',1)
+plot(x,A1+2*A2,x,P,x,M)
--- PAR3Models/PAR3AndPAR2.m
+++ PAR3Models/PAR3AndPAR2.m
@@ -11,13 +11,11 @@ ATot = 50;
 kpA = 2/ATot*koffA; % First number is Kp_hat
 kAplus = 60/500; % very unknown
 %PAR-2
-DP = 0.1;
-konP = 3*konA;
-koffP = koffA;
-PTot = ATot;
-kdpP = kdpA;
-kpP = kpA;
-rAP = 0.01; % A inhibiting P
+DP = 0.15;
+konP = 0.13;
+koffP = 7.3e-3;
+PTot = 50;
+rAP = 1e-2; % A inhibiting P
 rPA = rAP; % P inhibiting A
 % Dimensionless
 DA_Hat = DA/(L^2*koffA);
@@ -27,44 +25,41 @@ KpA_Hat = kpA*ATot/koffA;
 Kf_Hat = kAplus*ATot/konA;
 DP_Hat = DP/(L^2*koffA);
 KonP_Hat = konP/(koffA*h);
-KdpP_Hat = kdpP/koffA;
-KpP_Hat = kpP*PTot/koffA;
 KoffP_hat = koffP/koffA;
 
 % Initialization
-dt=1e-2;
+dt=1e-1;
 N=100;
 dx = 1/N;
 DSq = SecDerivMat(N,dx);
 x = (0:N-1)'*dx;
 % Start with small zone of PAR-2 on posterior cap
 cap = (x > 0.65 & x < 0.85);
-A1 = ones(N,1).*(~cap);
+A1 = 0.5*ones(N,1).*(~cap);
 A2 = 0.25*ones(N,1).*(~cap);
-P1 = ones(N,1).*cap;
-P2 = 0.25*ones(N,1).*cap;
+P = ones(N,1).*cap;
+plot(x,A1+2*A2,':',x,P,':')
+hold on
 
 er = 1;
 nIts = 0;
 while (er > 1e-10)
-    A1prev = A1; P1prev = P1; P2prev = P2; A2prev = A2;
+    A1prev = A1; A2prev = A2; Pprev = P;
     Ac = 1 - sum(A1+2*A2)*dx;
-    Pc = 1 - sum(P1+2*P2)*dx;
+    Pc = 1 - sum(P)*dx;
     Feedback = PAR3FeedbackFcn(A1+2*A2);
     RHS_A1 = KonA_Hat*(1+Kf_Hat*Feedback)*Ac - A1 + 2*KdpA_Hat*A2 - 2*KpA_Hat*A1.^2;
-    RHS_A2 = -KdpA_Hat*A2 + KpA_Hat*A1.^2 - rPA*PTot/koffA*(P1+2*P2).*A2;
-    RHS_P1 = KonP_Hat*Pc - KoffP_hat*P1 + 2*KdpP_Hat*P2 ...
-        - 2*KpP_Hat*P1.^2 - rAP*ATot/koffA*(A1+2*A2).*P1;
-    RHS_P2 = -KdpP_Hat*P2 + KpP_Hat*P1.^2 - rAP*ATot/koffA*(A1+2*A2).*P2;
-    P1 = (speye(N)/dt-DP_Hat*DSq) \ (P1/dt+RHS_P1);
-    P2 = P2 + dt*RHS_P2;
+    RHS_A2 = -KdpA_Hat*A2 + KpA_Hat*A1.^2 - rPA*PTot/koffA*P.*A2;
+    RHS_P = KonP_Hat*Pc - KoffP_hat*P - rAP*ATot/koffA*(A1+2*A2).*P;
+    P = (speye(N)/dt-DP_Hat*DSq) \ (P/dt+RHS_P);
     A1 = (speye(N)/dt-DA_Hat*DSq) \ (A1/dt+RHS_A1);
     A2 = A2 + dt*RHS_A2;
-    chk = (P1-P1prev)/dt- (DP_Hat*DSq*P1 + RHS_P1);
-    mv = [P1-P1prev; A1-A1prev; P2-P2prev; A2-A2prev]/dt;
+    chk = (P-Pprev)/dt- (DP_Hat*DSq*P + RHS_P);
+    mv = [P-Pprev; A1-A1prev; A2-A2prev]/dt;
     er = max(abs(mv));
     nIts=nIts+1;
     %plot(x,A1,x,2*A2,x,P1,x,2*P2)
     %drawnow
 end
-plot(x,A1,x,2*A2,x,P1,x,2*P2)
+set(gca,'ColorOrderIndex',1)
+plot(x,A1+2*A2,x,P)
