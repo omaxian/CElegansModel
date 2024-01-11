e2b8779a721a1e43a0432a400d146d3a5af25191
Ondrej Maxian <ondrejmaxian@pop-os.localdomain>
2024-01-10 18:01:58 -0600

    Active-inactive rho

--- EstablishmentPhase/RhoRGAEct2.m
+++ EstablishmentPhase/RhoRGAEct2.m
@@ -4,30 +4,28 @@ L = 134.6;
 koffM = 0.12; % set timescale
 D = 0.1/(L^2*koffM);
 % ECT-2 parameters
-KonE = 10;
-KoffE = 1/koffM;
+KonE = 0.5;
+KoffE = 0.5/koffM;
 % Rho parameters
-KonRho = 0.02;
+KonRho = 0.05;
 KoffRho = 1;
 % Myosin params
-KonM = 0.02;
+KonM = 0.1;
 ell = 0.1;
-Sigma0 = 2.6e-2;
+Sigma0 = 10*2.6e-2;
 % Reactions
-K_PE = 1;           % Rho recruiting ECT-2 (relative to basal)
-K_EP = 8/KonRho;    % ECT-2 recruiting Rho
-K_PM = 1/KonM;     % Rho producing actomyosin  
-K_ME = 0;          % Actomyosin recruiting ECT-2
-K_MP = 0.5;           % Actomyosin (RhoGAP) inhibiting Rho
+K_EPM = 1;           % Active rho and myosin recruiting ECT-2 (relative to basal)
+K_EP =  50;           % ECT-2  activating Rho
+K_MP = 5;           % Actomyosin (RhoGAP) inhibiting Rho (relative to basal)
+K_PM = 10;           % Rho producing actomyosin  
 Kfb = 0.1;
-KGAP = 1e-3;
 
 %% Numerical parameters
-dt = 1e-4;
-tf = 50;
-saveEvery = floor(0.1/dt);
+dt = 1e-2;
+tf = 60;
+saveEvery = floor(1/dt);
 nT = floor(tf/dt);
-N = 100;
+N = 1000;
 dx = 1/N;
 x = (0:N-1)'*dx;
 FracOff=0.1;
@@ -38,39 +36,42 @@ DSq = SecDerivMat(N,dx);
 DOneCenter = FirstDerivMatCenter(N,dx);
 DiffMat = (speye(N)/dt-D*DSq);
 [Diff_L,Diff_U,Diff_P]=lu(DiffMat);
-nSave = nT/saveEvery;
+nSave = floor(nT/saveEvery);
 AllMs = zeros(nSave,N);
-AllPs = zeros(nSave,N);
+AllPActs = zeros(nSave,N);
+AllPInActs = zeros(nSave,N);
 AllEs = zeros(nSave,N);
-%AllMs = zeros(nSave,N);
 
 % Initial guess
-P = KonRho/(KoffRho+KonRho)*ones(N,1);
+PActive = KonRho/(KonRho+KoffRho)*ones(N,1);
+PInActive = KonRho/(KonRho+KoffRho)*ones(N,1);
 M = KonM/(1+KonM)*ones(N,1);
 E = KonE/(KonE+KoffE)*ones(N,1);
 iFrame=0;
 f=figure;
+%tiledlayout(3,3,'Padding', 'none', 'TileSpacing', 'compact');
 for iT=0:nT
     t = iT*dt;
     if (mod(iT,saveEvery)==0)
         iFrame=iFrame+1;
-        plot(x,E,x,P,x,M)
+        plot(x,E,x,PInActive,x,PActive,x,M)
         title(strcat('$t=$',sprintf('%.2f', iT*dt/koffM),' s'))
         xlabel('\% egg length from anterior')
         xlim([0 1])
         xticks([0 1/4 1/2 3/4 1])
         xticklabels({'-50','-25','0','25','50'})
-        legend('ECT-2','Rho','Actomyosin/RGA','Location','southoutside','numColumns',3)
+        %legend('ECT-2','Rho','Actomyosin/RGA','Location','southoutside','numColumns',3)
         movieframes(iFrame)=getframe(f);
         AllMs(iFrame,:)=M;
-        AllPs(iFrame,:)=P;
+        AllPInActs(iFrame,:)=PInActive;
+        AllPActs(iFrame,:)=PActive;
         AllEs(iFrame,:)=E;
         %AllMs(iFrame,:)=M;
         %pause(0.5)
     end
     % 0) Cytoplasmic concentrations
     Ec = 1-sum(E)*dx;
-    Pc = 1-sum(P)*dx;
+    Pc = 1-sum(PActive+PInActive)*dx;
     Mc = 1-sum(M)*dx;
     % 1) Solve for velocity
     Sigma_active = ActiveStress(M);
@@ -78,18 +79,23 @@ for iT=0:nT
     vHalf = 1/2*(v+circshift(v,-1));
     % 2) Advection (explicit)
     MinusdxMv = Sigma0*AdvectionRHS(t,M,dx,vHalf,advorder);
-    MinusdxPv = Sigma0*AdvectionRHS(t,P,dx,vHalf,advorder);
+    MinusdxPInActv = Sigma0*AdvectionRHS(t,PInActive,dx,vHalf,advorder);
+    MinusdxPActv = Sigma0*AdvectionRHS(t,PActive,dx,vHalf,advorder);
     MinusdxEv = Sigma0*AdvectionRHS(t,E,dx,vHalf,advorder);
     % 3) Reaction
-    RHS_E = MinusdxEv + KonE*(~InPost + K_PE*P+K_ME*M)*Ec - KoffE*E;
-    RHS_P = MinusdxPv + KonRho*(1 + K_EP*max(E-0.5,0).*P./(Kfb+P))*Pc - KoffRho*(P+K_MP*M.*(P./(KGAP+P)));
-    RHS_M = MinusdxMv + KonM*(1 + K_PM*P.^3)*Mc - M;
-    P = Diff_U\ (Diff_L\(Diff_P*(P/dt+RHS_P)));
+    ActivatingRho = K_EP*E.*PActive./(Kfb+PActive).*PInActive + ...
+        -K_MP*M.*PActive;
+    RHS_E = MinusdxEv + KonE*(~InPost + K_EPM*PActive.*M)*Ec - KoffE*E;
+    RHS_PInAct = MinusdxPInActv + KonRho*Pc - KoffRho*PInActive - ActivatingRho;
+    RHS_PAct = MinusdxPActv + ActivatingRho;
+    RHS_M = MinusdxMv + KonM*(1 + K_PM*PActive)*Mc - M;
+    PActive = Diff_U\ (Diff_L\(Diff_P*(PActive/dt+RHS_PAct)));
+    PInActive = Diff_U\ (Diff_L\(Diff_P*(PInActive/dt+RHS_PInAct)));
     M = Diff_U\ (Diff_L\(Diff_P*(M/dt+RHS_M)));
     E = Diff_U\ (Diff_L\(Diff_P*(E/dt+RHS_E)));
 end
-ts=(0:nSave)*dt*saveEvery;
-plot(ts,AllEs(:,N/2),ts,AllPs(:,N/2),ts,AllMs(:,N/2))
+%ts=(0:nSave)*dt*saveEvery;
+%plot(ts,AllEs(:,N/2),ts,AllPInActs(:,N/2),ts,AllPActs(:,N/2),ts,AllMs(:,N/2))
 %hold on
 %set(gca,'ColorOrderIndex',1)
 %plot(ts,AllEs(:,1),':',ts,AllPs(:,1),':',ts,AllRs(:,1),':',ts,AllMs(:,N/2),':')
