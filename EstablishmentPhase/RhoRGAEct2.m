d79baec48d7aae2a5694ed7ba76e182d5cadf19e
Ondrej Maxian <ondrejmaxian@pop-os.localdomain>
2024-01-11 12:55:58 -0600

    Somewhat working

--- EstablishmentPhase/RhoRGAEct2.m
+++ EstablishmentPhase/RhoRGAEct2.m
@@ -10,26 +10,29 @@ KoffE = 0.5/koffM;
 KonRho = 0.05;
 KoffRho = 1;
 % Myosin params
-KonM = 0.1;
+KonM = 0.05;
 ell = 0.1;
-Sigma0 = 10*2.6e-2;
+Sigma0 = 2.6e-2*2;
 % Reactions
-K_EPM = 1;           % Active rho and myosin recruiting ECT-2 (relative to basal)
-K_EP =  50;           % ECT-2  activating Rho
-K_MP = 5;           % Actomyosin (RhoGAP) inhibiting Rho (relative to basal)
-K_PM = 10;           % Rho producing actomyosin  
+K_EPM = 2;           % Active rho and myosin recruiting ECT-2 (relative to basal)
+K_EP =  100;           % ECT-2  activating Rho
+K_MP = 0.5;           % Actomyosin (RhoGAP) inhibiting Rho (relative to basal)
+K_PM = 30;           % Rho producing actomyosin  
 Kfb = 0.1;
+KRhoMy=0.05;
+KERho = 0.1;
 
 %% Numerical parameters
-dt = 1e-2;
-tf = 60;
+dt = 1e-3;
+tf = 200;
 saveEvery = floor(1/dt);
 nT = floor(tf/dt);
-N = 1000;
+N = 100;
 dx = 1/N;
 x = (0:N-1)'*dx;
 FracOff=0.1;
-InPost = (x < FracOff/2 | x > 1-FracOff/2);
+InAnt = 1.0*~(x < FracOff/2 | x > 1-FracOff/2);
+InAnt(InAnt==0)=1/2;
 advorder = 1;
 % Second derivative matrix
 DSq = SecDerivMat(N,dx);
@@ -48,6 +51,7 @@ PInActive = KonRho/(KonRho+KoffRho)*ones(N,1);
 M = KonM/(1+KonM)*ones(N,1);
 E = KonE/(KonE+KoffE)*ones(N,1);
 iFrame=0;
+v=0;
 f=figure;
 %tiledlayout(3,3,'Padding', 'none', 'TileSpacing', 'compact');
 for iT=0:nT
@@ -66,7 +70,7 @@ for iT=0:nT
         AllPInActs(iFrame,:)=PInActive;
         AllPActs(iFrame,:)=PActive;
         AllEs(iFrame,:)=E;
-        %AllMs(iFrame,:)=M;
+        vmaxes(iFrame)=max(abs(v));
         %pause(0.5)
     end
     % 0) Cytoplasmic concentrations
@@ -82,13 +86,13 @@ for iT=0:nT
     MinusdxPInActv = Sigma0*AdvectionRHS(t,PInActive,dx,vHalf,advorder);
     MinusdxPActv = Sigma0*AdvectionRHS(t,PActive,dx,vHalf,advorder);
     MinusdxEv = Sigma0*AdvectionRHS(t,E,dx,vHalf,advorder);
-    % 3) Reaction
-    ActivatingRho = K_EP*E.*PActive./(Kfb+PActive).*PInActive + ...
+    % 3) Reaction (check these!)
+    ActivatingRho = K_EP*E.^2.*PActive./(Kfb+PActive).*PInActive + ...
         -K_MP*M.*PActive;
-    RHS_E = MinusdxEv + KonE*(~InPost + K_EPM*PActive.*M)*Ec - KoffE*E;
+    RHS_E = MinusdxEv + KonE*(InAnt + K_EPM*PActive.*M)*Ec - KoffE*E;
     RHS_PInAct = MinusdxPInActv + KonRho*Pc - KoffRho*PInActive - ActivatingRho;
     RHS_PAct = MinusdxPActv + ActivatingRho;
-    RHS_M = MinusdxMv + KonM*(1 + K_PM*PActive)*Mc - M;
+    RHS_M = MinusdxMv + KonM*(1 + K_PM*PActive./(KRhoMy+PActive))*Mc - M;
     PActive = Diff_U\ (Diff_L\(Diff_P*(PActive/dt+RHS_PAct)));
     PInActive = Diff_U\ (Diff_L\(Diff_P*(PInActive/dt+RHS_PInAct)));
     M = Diff_U\ (Diff_L\(Diff_P*(M/dt+RHS_M)));
